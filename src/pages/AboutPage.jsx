57932a539139044bafbb33ad5bc6a64d495133e6
Srush2566 <srushivpatil8380@gmail.com>
2025-06-13 09:14:02 +0530

    first commit

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
     
   <div style={{ padding: '1rem' }} className="home-scroll-container">
    
-    <h1 className='h1'>About Vivekanand College</h1>
+    <h1 className='new'>About Vivekanand College</h1>
     <p>Vivekanand College stands as a beacon of knowledge and integrity,committed to delivering transformative education since its inception in 1980. Located in the bustling heart of Kolhapur, our college has consistently strived to uphold the highest standards of academic rigor and ethical values.</p>
     <p>
       Our vision is to empower students with critical thinking skills, a global perspective, and a strong sense of social responsibility. 
